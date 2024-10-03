0d45c42cb27f715958dc9cbaaf71256c944ca489
Nikita Kharashun <tsurai7walkin@gmail.com>
2024-10-03 11:35:32 +0300

    Small fixes

--- Labs/Packet.cs
+++ Labs/Packet.cs
@@ -1,97 +1,98 @@
-namespace Labs;
-
-public class Packet
+namespace Labs
 {
-    public byte Flag { get; init; }
-    public int DestinationAddress { get; init; }
-    public int SourceAddress { get; init; }
-    public byte[] Data { get; init; }
-    public byte FCS { get; init; }
+    public class Packet
+    {
+        public byte[] Flag = [28, 0, 0, 0, 0, 0, 0, 0];
+        public int DestinationAddress { get; init; }
+        public int SourceAddress { get; init; }
+        public byte[] Data = new byte[29];
+        public byte FCS { get; init; }
 
-    public byte[] ToBytes()
-    {
-        var packetLength = 1 + 4 + 4 + Data.Length + 1;
-        var bytes = new byte[packetLength];
-        bytes[0] = Flag;
-        BitConverter.GetBytes(DestinationAddress).CopyTo(bytes, 1);
-        BitConverter.GetBytes(SourceAddress).CopyTo(bytes, 5);
-        Data.CopyTo(bytes, 9);
-        bytes[^1] = FCS;
-        return bytes;
-    }
-
-    public static Packet FromBytes(byte[] bytes)
-    {
-        var flag = bytes[0];
-        var destinationAddress = BitConverter.ToInt32(bytes, 1);
-        var sourceAddress = BitConverter.ToInt32(bytes, 5);
-        var data = new byte[bytes.Length - 10];
-        Array.Copy(bytes, 9, data, 0, data.Length);
-        var fcs = bytes[^1];
-        return new Packet
-        { 
-            Flag = flag,
-            DestinationAddress = destinationAddress,
-            SourceAddress = sourceAddress,
-            Data = data,
-            FCS = fcs 
-        };
-    }
-    
-    public static byte[] BitStuff(byte[] data)
-    {
-        var stuffedList = new List<byte>();
-        foreach (var b in data)
+        public byte[] ToBytes()
         {
-            if (b == 0x7E) 
-            {
-                stuffedList.Add(0x7D);
-                stuffedList.Add(0x5E);
-            }
-            else if (b == 0x7D) 
-            {
-                stuffedList.Add(0x7D);
-                stuffedList.Add(0x5D);
-            }
-            else
-            {
-                stuffedList.Add(b);
-            }
+            var packetLength = Flag.Length + 4 + 4 + Data.Length + 1;
+            var bytes = new byte[packetLength];
+
+            Array.Copy(Flag, 0, bytes, 0, Flag.Length); 
+            BitConverter.GetBytes(DestinationAddress).CopyTo(bytes, Flag.Length); 
+            BitConverter.GetBytes(SourceAddress).CopyTo(bytes, Flag.Length + 4);
+            Data.CopyTo(bytes, Flag.Length + 8); 
+            bytes[^1] = FCS;
+
+            return bytes;
         }
-        return stuffedList.ToArray();
-    }
 
-    public static byte[] BitUnstuff(byte[] data)
-    {
-        var unstuffedList = new List<byte>();
-        for (var i = 0; i < data.Length; i++)
+        public static Packet FromBytes(byte[] bytes)
         {
-            if (data[i] == 0x7D)
+            var flag = new byte[8];
+            Array.Copy(bytes, 0, flag, 0, 8);
+            var destinationAddress = BitConverter.ToInt32(bytes, 8);
+            var sourceAddress = BitConverter.ToInt32(bytes, 12);
+            var dataLength = bytes.Length - 17;
+            var data = new byte[dataLength];
+            Array.Copy(bytes, 16, data, 0, dataLength);
+            var fcs = bytes[^1];
+            
+            return new Packet
             {
-                if (i + 1 < data.Length)
+                Flag = flag,
+                DestinationAddress = destinationAddress,
+                SourceAddress = sourceAddress,
+                Data = data,
+                FCS = fcs
+            };
+        }
+
+        public static byte[] BitStuff(byte[] data)
+        {
+            var stuffedList = new List<byte>();
+            foreach (var b in data)
+            {
+                if (b == 0x7E)
                 {
-                    if (data[i + 1] == 0x5E)
-                    {
-                        unstuffedList.Add(0x7E);
-                        i++;
-                    }
-                    else if (data[i + 1] == 0x5D)
-                    {
-                        unstuffedList.Add(0x7D);
-                        i++;
-                    }
-                    else
-                    {
-                        unstuffedList.Add(data[i]);
-                    }
+                    stuffedList.Add(0x7D);
+                    stuffedList.Add(0x5E);
+                }
+                else if (b == 0x7D)
+                {
+                    stuffedList.Add(0x7D);
+                    stuffedList.Add(0x5D);
+                }
+                else
+                {
+                    stuffedList.Add(b);
                 }
             }
-            else
-            {
-                unstuffedList.Add(data[i]);
-            }
+            return stuffedList.ToArray();
         }
-        return unstuffedList.ToArray();
-    }
 
+        public static byte[] BitUnstuff(byte[] data)
+        {
+            var unstuffedList = new List<byte>();
+            for (var i = 0; i < data.Length; i++)
+            {
+                if (data[i] == 0x7D)
+                {
+                    if (i + 1 < data.Length)
+                    {
+                        if (data[i + 1] == 0x5E)
+                        {
+                            unstuffedList.Add(0x7E);
+                            i++;
+                        }
+                        else if (data[i + 1] == 0x5D)
+                        {
+                            unstuffedList.Add(0x7D);
+                            i++;
+                        }
+                    }
+                }
+                else
+                {
+                    unstuffedList.Add(data[i]);
+                }
+            }
+            return unstuffedList.ToArray();
+        }
+    }
 }
--- Labs/Program.cs
+++ Labs/Program.cs
@@ -16,10 +16,11 @@ while (true)
     await Task.Delay(3000);
     
     Console.WriteLine("1 - Transmit from First to Second");
-    Console.WriteLine("2 - Transmit from Fourth to Third");
+    Console.WriteLine("2 - Transmit from Second to First");
     Console.WriteLine("3 - Change COM port speed");
     Console.WriteLine("4 - Exit");
-    Console.WriteLine("Input: ");
+    Console.Write("Input: ");
+    
     var choice = int.Parse(Console.ReadLine()!);
 
     await (choice switch
@@ -31,26 +32,34 @@ while (true)
         _ => Task.Run(() => Console.WriteLine("Invalid input"))
     });
 
-    if (choice == 3) return;
+    if (choice == 4) return;
 }
 
-Task TransmitData(SerialPort senderPort, SerialPort receiverPort)
+async Task TransmitData(SerialPort senderPort, SerialPort receiverPort)
 {
+    var dataLength = 28 + 1;
+
     Console.WriteLine("Enter data to transmit: ");
     var input = Console.ReadLine()!;
     
+    if (input.Length > dataLength - 1)
+    {
+        Console.WriteLine($"Data length must be at most {dataLength - 1} characters.");
+        return;
+    }
+    
     var packet = new Packet
     {
-        Flag = 1,
-        SourceAddress = senderPort.PortName == "/dev/ttys001" ? 1 : 2,
-        DestinationAddress = senderPort.PortName == "/dev/ttys001" ? 2 : 1,
-        Data = Encoding.UTF8.GetBytes(input)
+        SourceAddress = senderPort.PortName[^1],
+        DestinationAddress = 0,
+        FCS = 0,
+        Data = Encoding.UTF8.GetBytes(input.PadRight(dataLength - 1, '\0'))
     };
     
     var packetBytes = packet.ToBytes();
     var stuffedBytes = Packet.BitStuff(packetBytes);
     
-    return Task.Run(() => senderPort.Write(stuffedBytes, 0, stuffedBytes.Length));
+    await Task.Run(() => senderPort.Write(stuffedBytes, 0, stuffedBytes.Length));
 }
 
 Task ChangeSpeed(SerialPort port1, SerialPort port2)
@@ -73,14 +82,53 @@ void HandleDataReceived(SerialPort port, string portName)
 {
     var data = port.ReadExisting();
     var bytes = Encoding.UTF8.GetBytes(data);
+
+
+    var originalBytes = bytes.ToArray(); 
     
     var unstuffedBytes = Packet.BitUnstuff(bytes);
     var packet = Packet.FromBytes(unstuffedBytes);
-    
+
     Console.WriteLine($"Data received from {portName}:");
-    Console.WriteLine($"  Flag: {packet.Flag}");
-    Console.WriteLine($"  Source Address: {packet.SourceAddress}");
-    Console.WriteLine($"  Destination Address: {packet.DestinationAddress}");
-    Console.WriteLine($"  Data: {Encoding.UTF8.GetString(packet.Data)}");
     
+    Console.WriteLine("Original Frame (before de-stuffing):");
+    PrintFrame(originalBytes, "Original");
+    
+    Console.WriteLine("Decoded Packet:");
+    PrintFrame(unstuffedBytes, "Decoded");
+
+    var flagBinary = string.Join(", ", packet.Flag.Select(b => Convert.ToString(b, 2).PadLeft(8, '0')));
+    Console.WriteLine($"  Flag: [{flagBinary}]");
+    
+    var sourceAddressHex = $"0x{packet.SourceAddress:X}";
+    var destinationAddressHex = $"0x{packet.DestinationAddress:X}";
+    
+    Console.WriteLine($"  Source Address: {sourceAddressHex}");
+    Console.WriteLine($"  Destination Address: {destinationAddressHex}");
+    
+    var dataAddresses = string.Join(", ", packet.Data.Select(b => $"0x{b:X2}")); 
+    Console.WriteLine($"  Data: [{dataAddresses}]");
+}
+
+void PrintFrame(byte[] frame, string label)
+{
+    Console.WriteLine($"{label} Frame:");
+
+    for (int i = 0; i < frame.Length; i++)
+    {
+        string binaryString = Convert.ToString(frame[i], 2).PadLeft(8, '0');
+        
+        if (frame[i] == 0x7D) 
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"  Byte {i}: {binaryString} (Modified)");
+            Console.ResetColor();
+        }
+        else
+        {
+            Console.WriteLine($"  Byte {i}: {binaryString}");
+        }
+    }
+
+    Console.WriteLine();
 }
